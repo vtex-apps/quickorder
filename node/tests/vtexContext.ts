45716fdd34bc3451739832e9c6740a3a5e5ae2ef
polishq <arthur.bond@vtex.com.br>
2022-06-13 11:34:14 -0400

    Revise test config

--- node/tests/vtexContext.ts
+++ node/tests/vtexContext.ts
@@ -1,14 +1,10 @@
 import type { IOContext } from '@vtex/api'
 
 import Service from '../index'
+import { resolvers } from '../resolvers'
 import { Search } from '../clients/search'
 
-type GraphQLResolver = {
-  resolvers: any
-}
-
 const vtexContext = () => {
-  const { resolvers } = Service.config.graphql as GraphQLResolver
   const { Query } = resolvers
   const context = {} as IOContext
   const clients = {
