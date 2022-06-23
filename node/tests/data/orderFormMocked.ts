ee7e41841ae3bd3273b894cf4824638402dafb15
polishq <arthur.bond@vtex.com.br>
2022-06-23 16:37:43 -0400

    Use vtex-test-tools instead of jest

--- node/tests/data/orderFormMocked.ts
+++
@@ -1,9 +0,0 @@
-const orderFormMocked = () => ({
-  salesChannel: '1',
-  storePreferencesData: {
-    countryCode: 'BR',
-  },
-  shippingData: {},
-})
-
-export default orderFormMocked
--- node/tests/data/postStockMocked.ts
+++
@@ -1,8 +0,0 @@
-const postStockMocked = () => ({
-  'D25133K-B2': 100,
-  '14-556': 5,
-  '123': 100,
-  'DCF880L2-BR': 1515,
-})
-
-export default postStockMocked
