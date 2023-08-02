acad8c4e0faaecf012e9d75e4f105a050dfc125e
Alberto Mendoza <albertm_805@outlook.com>
2023-08-01 20:39:56 -0600

    Update quickorder ui buttons and text

---
+++ react/AddMoreLinesButton.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Button } from 'vtex.styleguide'
+// import styles from './global.css'
+
+
+const AddMoreLinesButton = () => {
+    return (
+        <div>
+            <Button>+ Add More Lines</Button>
+        </div>
+    );
+};
+
+export default AddMoreLinesButton;
