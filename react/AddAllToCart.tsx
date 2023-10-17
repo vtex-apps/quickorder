cc9dae94ce2c1ce6b77e1bef6dcdac6e421cb9f5
Alberto Mendoza <albertm_805@outlook.com>
2023-10-17 07:59:50 -0600

    Add all to list button removed

--- react/AddAllToCart.tsx
+++ react/AddAllToCart.tsx
@@ -3,14 +3,19 @@ import { useCssHandles } from 'vtex.css-handles'
 import { Button } from 'vtex.styleguide'
 import './global.css'
 
-const AddAllToCart = () => {
+interface AddAllToCart {
+  onClick: () => void;
+  isLoading: boolean;
+}
+
+const AddAllToCart: React.FC<AddAllToCart> = ({ onClick }) => {
   const CSS_HANDLES = ['addAllToCartContainer', 'addAllToCartButton'] as const
 
   const handles = useCssHandles(CSS_HANDLES)
 
   return (
     <div className={handles.addAllToCartContainer}>
-      <Button variation="primary"> ADD ALL TO CART</Button>
+      <Button isLoading={false} onClick={onClick} variation="primary"> ADD ALL TO CART</Button>
     </div>
   )
 }
