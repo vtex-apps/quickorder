291279f76298043a857e0f8587c2f7318d78e80a
Emanuel Ramos <emanuelramos@MacBook-Air-de-Emanuel.local>
2024-05-22 09:49:31 -0300

    fix: create a function component to dropzone Icon

---
+++ react/assets/DropZoneIcon.tsx
@@ -0,0 +1,4 @@
+import React from "react"
+export const DropzoneIcon = ({className}) => {
+    return <svg className={className} width="40" height="46" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M24.793.793A1 1 0 0 0 24.086.5H1.5a1 1 0 0 0-1 1v43a1 1 0 0 0 1 1h37a1 1 0 0 0 1-1V15.914a1 1 0 0 0-.293-.707L24.793.793z" fill="#E3E4E6"></path><path d="M9.5 33.5h21M9.5 24.5h21" stroke="#3F3F40" stroke-width="1.2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"></path><path d="M9.5 15.5h6" stroke="#F71963" stroke-width="1.2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"></path><path d="M24.5.5v15h15" fill="#CACBCC"></path></svg>
+}
